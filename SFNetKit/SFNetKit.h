fd5af83daf74933fff526e94c20dbe04966448ba
Ivan Shokurov <schokuroff@icloud.com>
2021-07-23 03:43:15 +0800

    added a blank xcode framework template

---
+++ SFNetKit/SFNetKit.h
@@ -0,0 +1,18 @@
+//
+//  SFNetKit.h
+//  SFNetKit
+//
+//  Created by Ivan Shokurov (G1) on 23.07.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SFNetKit.
+FOUNDATION_EXPORT double SFNetKitVersionNumber;
+
+//! Project version string for SFNetKit.
+FOUNDATION_EXPORT const unsigned char SFNetKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFNetKit/PublicHeader.h>
+
+
