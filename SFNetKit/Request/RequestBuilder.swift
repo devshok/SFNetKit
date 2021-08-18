392cf6787b5ebdde9e9c91b6d626c403b7ea61e4
Ivan Shokurov <schokuroff@icloud.com>
2021-08-18 15:00:18 +0800

    f/settings: changed cache policy requirements for network requests

--- SFNetKit/Request/RequestBuilder.swift
+++ SFNetKit/Request/RequestBuilder.swift
@@ -9,6 +9,16 @@ struct RequestBuilder {
     // MARK: - Builder
     
     func build(with configuration: RequestConfiguration) -> URLRequest? {
+        defer {
+            let cache = URLCache.shared
+            let memoryKilobytes = Double(cache.memoryCapacity / 1024)
+            let memory = "memory capacity: \(memoryKilobytes) KB"
+            let diskKilobytes = Double(cache.diskCapacity / 1024)
+            let disk = "disk capacity: \(diskKilobytes) KB"
+            let currentKilobytes = Double(cache.currentDiskUsage / 1024)
+            let current = "current disk usage: \(currentKilobytes) KB"
+            debugPrint("RequestBuilder", #function, #line, memory, disk, current)
+        }
         let components: URLComponents = {
             var c = URLComponents()
             c.scheme = configuration.scheme
@@ -17,7 +27,7 @@ struct RequestBuilder {
             return c
         }()
         guard let url = components.url else { return nil }
-        var request = URLRequest(url: url)
+        var request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad)
         request.httpMethod = configuration.method
         return request
     }
